db5f388f0128ca5101d8bc2752850996bacb55ee
Yumen Cao <gawain@yumentsao.com>
2015-04-01 21:37:50 -0500

    mp2

--- Stepper/ViewController.swift
+++ Stepper/ViewController.swift
@@ -9,20 +9,100 @@
 import UIKit
 import Foundation
 import CoreMotion
+import CoreLocation
+import AVFoundation
 import MessageUI
 
-class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
+let precisionDefault:Double = 12
+let strideDefault:Double = 0.38
+
+class ViewController: UIViewController, MFMailComposeViewControllerDelegate, CLLocationManagerDelegate, AVAudioRecorderDelegate,AVAudioPlayerDelegate {
+    
+    @IBOutlet weak var precisionInput: UITextField!
+    @IBOutlet weak var strideInput: UITextField!
     
     @IBOutlet weak var gyroscopeLabel: UILabel!
     @IBOutlet weak var accelerometerLabel: UILabel!
     @IBOutlet weak var attitudeLabel: UILabel!
     @IBOutlet weak var stepCounter: UILabel!
+    @IBOutlet weak var northPole: UILabel!
+    @IBOutlet weak var soundLevel: UILabel!
+    @IBOutlet weak var totalDegree: UILabel!
     
     @IBAction func doneRecording(sender: AnyObject) {
+        startButton.enabled = true
+        doneButton.enabled = false
+
+        recorder?.stop()
+        
+        if motionManager.deviceMotionAvailable {
+            motionManager.stopDeviceMotionUpdates()
+            updateTimer.invalidate()
+            updateTimer = nil
+        }
+        if CLLocationManager.headingAvailable() {
+            locationManager.stopUpdatingHeading()
+        }
+        
         sendMail(output)
+        
+    }
+    
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var doneButton: UIButton!
+    
+    @IBAction func startEverything(sender: AnyObject) {
+        startButton.enabled = false
+        doneButton.enabled = true
+        
+        var precisionTemp = (precisionInput.text as NSString).doubleValue
+        if precisionTemp > 0 {
+            self.precision = precisionTemp
+        }
+        
+        var strideTemp = (strideInput.text as NSString).doubleValue
+        if strideTemp > 0 {
+            self.stride = strideTemp
+        }
+        
+        precisionInput.text = String(format: "Precision: %.0f", self.precision)
+        strideInput.text = String(format: "Stride: %.3f", self.stride)
+        
+        self.step = 3.141592653/self.precision
+        
+        recorder?.record()
+        
+        now = NSDate()
+        if motionManager.deviceMotionAvailable {
+            motionManager.deviceMotionUpdateInterval = interval
+            motionManager.startDeviceMotionUpdates()
+            
+        }
+        if CLLocationManager.headingAvailable() {
+            locationManager.startUpdatingHeading()
+        }
+        
+        updateTimer = NSTimer.scheduledTimerWithTimeInterval(interval, target: self, selector: "updateDisplay", userInfo: nil, repeats: true)
+
     }
     
     private let motionManager = CMMotionManager()
+    private let locationManager = CLLocationManager()
+    
+    private var recorder:AVAudioRecorder?
+    
+    private var compassGeoFlt:Float = -1
+    private var compassMagFlt:Float = -1
+    private var soundChl1Flt:Float = 1
+    private var soundChl2Flt:Float = 1
+    
+    // total degree
+    private var precision:Double = precisionDefault
+    private var stride:Double = strideDefault
+    private var step:Double = 3.141592653/precisionDefault
+    private var inital:Double = 0
+    private var totalDegreeTurned: Double = 0
+    
     private var updateTimer:NSTimer!
     
     private let bundle = NSBundle.mainBundle()
@@ -38,8 +118,14 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
     var output : NSString = ""
     var now : NSDate?
     
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        self.view.endEditing(true)
+    }
+    
     func updateDisplay() {
         let motion = motionManager.deviceMotion
+        let heading = locationManager.heading
+        
         if motion != nil {
             
             let rotationRate = motion.rotationRate
@@ -51,10 +137,12 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
             let acceleratorText = String(format:"Acceleration:\nGravity x: %+.2f User x: %+.2f\nGravity y: %+.2f User y: %+.2f\nGravity z: %+.2f User z: %+.2f", gravity.x, userAcc.x, gravity.y, userAcc.y, gravity.z, userAcc.z)
             let attitudeText = String(format:"Attitude:\nRoll: %+.2f  Pitch: %+.2f Yaw:%+.2f", attitude.roll, attitude.pitch, attitude.yaw)
             
-            let timestamp = now!.timeIntervalSinceNow
-            let timestampStr = String("\(-timestamp), ")
             
-            let content = String(format:"%+.2f, %+.2f, %+.2f, %+.2f, %+.2f, %+.2f, %+.2f, %+.2f, %+.2f, %+.2f, %+.2f, %+.2f\n", rotationRate.x, rotationRate.y, rotationRate.z, gravity.x, userAcc.x, gravity.y, userAcc.y, gravity.z, userAcc.z, attitude.roll, attitude.pitch, attitude.yaw)
+            let timestamp = now!.timeIntervalSinceNow
+            let timestampStr = String(format:"%.3f,", -timestamp)
+            
+            let content = String(format:"%+.2f, %+.2f, %+.2f, %+.2f, %+.2f, %+.2f, %+.2f, %+.2f, %+.2f, %+.2f, %+.2f, %+.2f, %+.2f, %+.2f, %+.2f, %+.2f\n", rotationRate.x, rotationRate.y, rotationRate.z, gravity.x, userAcc.x, gravity.y, userAcc.y, gravity.z, userAcc.z, attitude.roll, attitude.pitch, attitude.yaw, soundChl1Flt, soundChl2Flt, compassGeoFlt, compassMagFlt)
+            
             output = output + timestampStr + content
             
             if (Double(userAcc.z) - lowest) > stepThreshold && stepFlag == 0 {
@@ -67,13 +155,37 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
             }
             
             dispatch_async(dispatch_get_main_queue(), {
-                    self.gyroscopeLabel.text = gyroscopText
-                    self.accelerometerLabel.text = acceleratorText
-                    self.attitudeLabel.text = attitudeText
-                    self.stepCounter.text = String("Steps: \(self.stepCount)\nTime: \(-timestamp)")
-            })
+                self.gyroscopeLabel.text = gyroscopText
+                self.accelerometerLabel.text = acceleratorText
+                self.attitudeLabel.text = attitudeText
+                self.stepCounter.text = String("Steps: \(self.stepCount) Distance: \(Double(self.stepCount)*self.stride)\nTime: \(timestampStr)")
+                
+                self.calculateDegreeTurned(attitude.yaw)
 
+                if self.recorder?.recording == true {
+                    self.recorder?.updateMeters()
+                    
+                    self.soundChl1Flt = self.recorder!.peakPowerForChannel(0)
+                    self.soundChl2Flt = self.recorder!.peakPowerForChannel(1)
+                    self.soundLevel.text = String(format: "Sound Level: %.2f %.2f", self.soundChl1Flt, self.soundChl2Flt)
+                                    }
+            })
         }
+    }
+    
+    func calculateDegreeTurned( yaw:Double ) {
+        if abs(yaw - self.inital) > 2 {
+            self.inital = -1 * self.inital
+        }
+        if yaw > (self.inital + self.step) {
+            self.totalDegreeTurned = self.totalDegreeTurned + 180/self.precision
+            self.inital = self.inital + self.step
+        }
+        else if yaw < (self.inital - self.step) {
+            self.totalDegreeTurned = self.totalDegreeTurned + 180/self.precision
+            self.inital = self.inital - self.step
+        }
+        totalDegree.text = String(format: "Total Degree Turned: %.2f", self.totalDegreeTurned)
     }
     
     func sendMail( body:NSString) -> Int {
@@ -90,7 +202,6 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
     
         picker.setToRecipients(toRecipients)
         self.presentViewController(picker, animated:true, completion:nil)
-        NSLog("sent")
         return 0
     }
     
@@ -99,32 +210,73 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
         self.dismissViewControllerAnimated(true, completion:nil)
     }
     
-    
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        now = NSDate()
-        if motionManager.deviceMotionAvailable {
-            
-            motionManager.deviceMotionUpdateInterval = interval
-            motionManager.startDeviceMotionUpdates()
-            updateTimer = NSTimer.scheduledTimerWithTimeInterval(interval, target: self, selector: "updateDisplay", userInfo: nil, repeats: true)
+    func locationManager(manager: CLLocationManager!, didUpdateHeading newHeading: CLHeading!) {
+        if newHeading.headingAccuracy < 0 {
+            return
         }
-
+        
+        self.northPole.text = String(format:"GeoNP: %.2f MagNP: %.2f", newHeading.trueHeading,newHeading.magneticHeading)
+        self.compassGeoFlt = Float(newHeading.trueHeading)
+        self.compassMagFlt = Float(newHeading.magneticHeading)
+        return
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.headingFilter = kCLHeadingFilterNone
+        
+        let dirPaths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        let docsDir = dirPaths[0] as String
+        let soundFilePath = docsDir.stringByAppendingPathComponent("sound.caf")
+        let soundFileURL = NSURL(fileURLWithPath: soundFilePath)
+        let recordSettings = [AVEncoderAudioQualityKey: AVAudioQuality.Min.rawValue, AVEncoderBitRateKey: 16,AVNumberOfChannelsKey: 2, AVSampleRateKey: 44100.0]
+        
+        var error: NSError?
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        audioSession.setCategory(AVAudioSessionCategoryPlayAndRecord,
+            error: &error)
+        
+        if let err = error {
+            println("audioSession error: \(err.localizedDescription)")
+        }
+        
+        recorder = AVAudioRecorder(URL: soundFileURL,
+            settings: recordSettings, error: &error)
+        
+        if let err = error {
+            println("audioSession error: \(err.localizedDescription)")
+        } else {
+            recorder?.prepareToRecord()
+        }
+        recorder?.meteringEnabled = true
+        doneButton.enabled = false
     }
     
-    override func viewDidDisappear(animated: Bool) {
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
         super.viewDidDisappear(animated)
-        if motionManager.deviceMotionAvailable {
-            
-            motionManager.stopDeviceMotionUpdates()
-            updateTimer.invalidate()
-            updateTimer = nil
-        }
+        
+    }
+    
+    func audioPlayerDidFinishPlaying(player: AVAudioPlayer!, successfully flag: Bool) {
+    }
+    
+    func audioPlayerDecodeErrorDidOccur(player: AVAudioPlayer!, error: NSError!) {
+        println("Audio Play Decode Error")
+    }
+    
+    func audioRecorderDidFinishRecording(recorder: AVAudioRecorder!, successfully flag: Bool) {
+    }
+    
+    func audioRecorderEncodeErrorDidOccur(recorder: AVAudioRecorder!, error: NSError!) {
+        println("Audio Record Encode Error")
     }
     
     override func shouldAutorotate() -> Bool {
@@ -135,7 +287,5 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
